558f0ea8e7976f790b8e0f7ad928affd46405939
Kristaps Grinbergs <kristaps@qminderapp.com>
2016-01-30 18:24:55 +0200

    #8 Show coordinates and navigate to

--- Dodies.lv/Details.swift
+++ Dodies.lv/Details.swift
@@ -17,6 +17,7 @@ class Details : UIViewController {
   @IBOutlet weak var desc: UITextView!
   @IBOutlet weak var closeButton: UIButton!
   @IBOutlet weak var descriptionLinkButton: UIButton!
+  @IBOutlet weak var coordinatesButton: UIButton!
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -26,6 +27,7 @@ class Details : UIViewController {
     
     closeButton.layer.cornerRadius = 5
     descriptionLinkButton.layer.cornerRadius = 5
+    coordinatesButton.titleLabel?.text = "   \(point.coordinate.latitude),\(point.coordinate.longitude)"
     
     if point.apraksts == "true" {
       descriptionLinkButton.hidden = false
@@ -40,4 +42,12 @@ class Details : UIViewController {
   @IBAction func showDescription(sender: AnyObject) {
     UIApplication.sharedApplication().openURL(NSURL(string: "http://dodies.lv/info?taka=\(point.id)")!)
   }
+  
+  @IBAction func openNavigation(sender: AnyObject) {
+    let u = "http://maps.apple.com/?daddr=\(point.coordinate.latitude),\(point.coordinate.longitude)"
+    print(u)
+    UIApplication.sharedApplication().openURL(NSURL(string: u)!)
+
+  }
+  
 }
