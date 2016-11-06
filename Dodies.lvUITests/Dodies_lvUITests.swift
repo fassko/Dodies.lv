55c7d704dcb07d3b583d4bbee50b46fa79507eb6
Kristaps Grinbergs <fassko@gmail.com>
2016-11-06 22:02:26 +0200

    Hope

---
+++ Dodies.lvUITests/Dodies_lvUITests.swift
@@ -0,0 +1,27 @@
+//
+//  Dodies_lvUITests.swift
+//  Dodies.lvUITests
+//
+//  Created by Kristaps Grinbergs on 06/11/2016.
+//  Copyright © 2016 fassko. All rights reserved.
+//
+
+import XCTest
+
+class Dodies_lvUITests: XCTestCase {
+        
+  override func setUp() {
+    super.setUp()
+    
+    let app = XCUIApplication()
+    setupSnapshot(app)
+    app.launch()
+  }
+  
+  func testExample() {
+    sleep(3)
+    XCUIApplication().alerts["Allow “Dodies.lv” to access your location while you use the app?"].buttons["Allow"].tap()
+    
+  }
+    
+}
