8fab5053a9add3ebc9e1956e92ea3d580989683e
Kristaps Grinbergs <fassko@gmail.com>
2018-01-06 19:36:23 +0200

    Test run with iPhone and iPad.

--- Dodies.lvUITests/Dodies_lvUITests.swift
+++ Dodies.lvUITests/Dodies_lvUITests.swift
@@ -24,36 +24,7 @@ class Dodies_lvUITests: XCTestCase {
   }
   
   func testExample() {
-    sleep(3)
-//    XCUIApplication().alerts["Allow “Dodies.lv” to access your location while you are using the app?"].buttons["Allow"].tap()
-    snapshot("App launched")
-    
-    let app = XCUIApplication()
-    
-    let b = app.otherElements["Map"].buttons["Ogres zilie kalni"]
-    
-    
-    b.tap()
-    
-    sleep(10)
-    
-    print(app.debugDescription)
-    
-    
-    
-
-
-//    app.buttons["More info"].tap()
-    
-//    app/*@START_MENU_TOKEN@*/.otherElements["Map"].buttons["Ogres zilie kalni"]/*[[".otherElements[\"Map\"].buttons[\"Ogres zilie kalni\"]",".buttons[\"Ogres zilie kalni\"]"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/.tap()
-    
-//    let mapButton = app/*@START_MENU_TOKEN@*/.buttons["Map"]/*[[".otherElements[\"Map\"].buttons[\"Map\"]",".buttons[\"Map\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-//    mapButton.tap()
-//    mapButton.tap()
-//    app/*@START_MENU_TOKEN@*/.otherElements["Map"].buttons["More Info"]/*[[".otherElements[\"Map\"].buttons[\"More Info\"]",".buttons[\"More Info\"]"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/.tap()
-    
-//    XCUIApplication().alerts["Allow “Dodies.lv” to access your location while you use the app?"].buttons["Allow"].tap()
-    
+    sleep(5)
   }
     
 }
