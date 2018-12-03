ec49df55505b86578e1a52ad6c9f8d8b029c159b
Kristaps Grinbergs <fassko@gmail.com>
2018-12-03 22:20:40 +0200

    Improve clustering

--- Dodies.lv/ClusterAnnotationView.swift
+++ Dodies.lv/ClusterAnnotationView.swift
@@ -9,51 +9,32 @@
 import Foundation
 import MapKit
 
-class ClusterAnnotationView: MKAnnotationView {
+class ClusterAnnotationView: MKMarkerAnnotationView {
   
   override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
     super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
-    collisionMode = .circle
-    centerOffset = CGPoint(x: 0, y: -10)
+    displayPriority = .defaultHigh
+    canShowCallout = false
   }
   
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
   
-  override func prepareForDisplay() {
-    super.prepareForDisplay()
-    
-    if let cluster = annotation as? MKClusterAnnotation {
-      let total = cluster.memberAnnotations.count
-      
-      let color: UIColor = {
-        if let firstAnnotation = cluster.memberAnnotations.first as? DodiesAnnotation {
-          return firstAnnotation.color
-        }
-        return #colorLiteral(red: 0.4274509804, green: 0.5921568627, blue: 0.2549019608, alpha: 1)
-      }()
-      
-      image = {
-        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 40, height: 40))
+  override var annotation: MKAnnotation? {
+    willSet {
+      if let cluster = newValue as? MKClusterAnnotation {
         
-        return renderer.image { _ in
-          color.setFill()
-          UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: 40, height: 40)).fill()
-          
-          UIColor.white.setFill()
-          UIBezierPath(ovalIn: CGRect(x: 8, y: 8, width: 24, height: 24)).fill()
-          
-          let attributes = [
-            NSAttributedString.Key.foregroundColor: UIColor.black,
-            NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18)
-          ]
-          let text = "\(total)"
-          let size = text.size(withAttributes: attributes)
-          let rect = CGRect(x: 20 - size.width / 2, y: 20 - size.height / 2, width: size.width, height: size.height)
-          text.draw(in: rect, withAttributes: attributes)
-        }
-      }()
+        markerTintColor = {
+          if let firstAnnotation = cluster.memberAnnotations.first as? DodiesAnnotation {
+            return firstAnnotation.color
+          }
+          return #colorLiteral(red: 0.4274509804, green: 0.5921568627, blue: 0.2549019608, alpha: 1)
+        }()
+        
+        glyphText = "\(cluster.memberAnnotations.count)"
+      
+      }
     }
   }
 }
--- Dodies.lv/Constants.swift
+++ Dodies.lv/Constants.swift
@@ -18,7 +18,7 @@ enum Constants {
   static let towerColor = #colorLiteral(red: 0.3960784314, green: 0.7058823529, blue: 0.7333333333, alpha: 1)
   static let picnicColor = #colorLiteral(red: 0.9294117647, green: 0.4196078431, blue: 0.003921568627, alpha: 1)
   
-  static let trailImage = #imageLiteral(resourceName: "trail")
-  static let towerImage = #imageLiteral(resourceName: "binoculars")
+  static let trailImage = #imageLiteral(resourceName: "signpost")
+  static let towerImage = #imageLiteral(resourceName: "binoculars.pdf")
   static let picnicImage = #imageLiteral(resourceName: "picnic-table")
 }
